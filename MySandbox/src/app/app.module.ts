f4802c9ed22cc35be0013f813351a1f3981021ed
Anonymous <simowned@hotmail.com>
2019-05-20 21:40:00 +0200

    navigation components

--- MySandbox/src/app/app.module.ts
+++ MySandbox/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { appRoutes } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { NewsComponent } from './news/news.component';
+import { FindUsComponent } from './find-us/find-us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
+    TeamComponent,
+    NewsComponent,
+    FindUsComponent,
+    PageNotFoundComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
