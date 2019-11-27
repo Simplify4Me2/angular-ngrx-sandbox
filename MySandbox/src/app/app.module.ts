e46401207c8bcb9c49384412c0036a0b70d77970
Simon <simon.vereecke@live.com>
2019-11-27 21:55:39 +0100

    Starting from scratch

--- MySandbox/src/app/app.module.ts
+++ MySandbox/src/app/app.module.ts
@@ -4,25 +4,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { appRoutes } from './app.routes';
 import { HomeComponent } from './home/home.component';
-import { TeamComponent } from './team/team.component';
-import { NewsComponent } from './news/news.component';
-import { FindUsComponent } from './find-us/find-us.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
     HomeComponent,
-    TeamComponent,
-    NewsComponent,
-    FindUsComponent,
-    PageNotFoundComponent,
-    AboutUsComponent
   ],
   imports: [
     BrowserModule,
--- MySandbox/src/app/app.routes.ts
+++ MySandbox/src/app/app.routes.ts
@@ -1,14 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { TeamComponent } from './team/team.component';
-import { NewsComponent } from './news/news.component';
-import { FindUsComponent } from './find-us/find-us.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'team', component: TeamComponent },
-    { path: 'news', component: NewsComponent },
-    { path: 'findus', component: FindUsComponent },
-    { path: '**', component: PageNotFoundComponent },    
+    { path: 'home', component: HomeComponent }
   ];
