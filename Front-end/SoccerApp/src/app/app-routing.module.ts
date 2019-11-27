035917ca1af9636774ab9ca9d7339ae10960ae9d
Simon <simon.vereecke@live.com>
2019-11-27 22:17:29 +0100

    Create match component

--- Front-end/SoccerApp/src/app/app-routing.module.ts
+++ Front-end/SoccerApp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MatchComponent } from './match/match.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'match', component: MatchComponent },
+  { path: '', redirectTo: 'match', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
