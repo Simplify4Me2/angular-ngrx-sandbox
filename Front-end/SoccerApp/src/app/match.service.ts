8df245a415d3418d507f582a9ec123ff945e4c7a
Simon <simon.vereecke@live.com>
2019-11-30 23:36:26 +0100

    backend call

--- Front-end/SoccerApp/src/app/match.service.ts
+++ Front-end/SoccerApp/src/app/match.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Match } from './shared/match.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchService {
 
-  get(): Match[] {
-    return  [
-      { nr: 1, homeTeam: 'FC De Kampioenen', awayTeam: 'VK Heuvel Lo', homeScore: 3, awayScore: 1 },
-      { nr: 2, homeTeam: 'RSC Anderlecht', awayTeam: 'STVV', homeScore: 2, awayScore: 2 },
-      { nr: 3, homeTeam: 'Zulte Waregem', awayTeam: 'Cercle Brugge', homeScore: 4, awayScore: 0 },
-      { nr: 4, homeTeam: 'RC Genk', awayTeam: 'OHL', homeScore: 0, awayScore: 1 }];
+  constructor(private http: HttpClient) {}
+
+  get(): Promise<Match[]> {
+
+    return this.http.get<Match[]>('localhost:3000/match')
+      .toPromise();
+
+    // return  [
+    //   { nr: 1, homeTeam: 'FC De Kampioenen', awayTeam: 'VK Heuvel Lo', homeScore: 3, awayScore: 1 },
+    //   { nr: 2, homeTeam: 'RSC Anderlecht', awayTeam: 'STVV', homeScore: 2, awayScore: 2 },
+    //   { nr: 3, homeTeam: 'Zulte Waregem', awayTeam: 'Cercle Brugge', homeScore: 4, awayScore: 0 },
+    //   { nr: 4, homeTeam: 'RC Genk', awayTeam: 'OHL', homeScore: 0, awayScore: 1 }];
   }
 
 }
